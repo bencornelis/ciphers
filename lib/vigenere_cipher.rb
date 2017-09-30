0b4fda22dbec66bd3d136c806c3604a12c264bcc
Ben Cornelis <bencornelis02@gmail.com>
2017-09-30 15:29:03 -0700

    add vigenere cipher

---
+++ lib/vigenere_cipher.rb
@@ -0,0 +1,45 @@
+class Cipher
+  ALPHABET = ('a'..'z').to_a
+  PLACES   = (0..25).to_a
+
+  attr_reader :key, :letter_to_place
+
+  def initialize(key=generate_key)
+    validate(key)
+
+    @key = key
+    @letter_to_place = ALPHABET.zip(PLACES).to_h
+  end
+
+  def encode(plaintext)
+    plaintext.gsub(/./).with_index { |letter, idx| encode_letter(letter, idx) }
+  end
+
+  def decode(ciphertext)
+    ciphertext.gsub(/./).with_index { |letter, idx| decode_letter(letter, idx) }
+  end
+
+  private
+
+  def validate(key)
+    raise ArgumentError, 'Bad key' unless key =~ /\A[a-z]+\z/
+  end
+
+  def generate_key
+    100.times.map { ALPHABET.sample }.join
+  end
+
+  def encode_letter(letter, idx)
+    encoded_idx = (letter_to_place[letter] + shift_at(idx)) % 26
+    letter_to_place.invert[encoded_idx]
+  end
+
+  def decode_letter(letter, idx)
+    decoded_idx = (letter_to_place[letter] - shift_at(idx) + 26) % 26
+    letter_to_place.invert[decoded_idx]
+  end
+
+  def shift_at(idx)
+    letter_to_place[key[idx % key.length]]
+  end
+end
---
+++ spec/vigenere_cipher_spec.rb
@@ -0,0 +1,63 @@
+require 'rspec'
+require 'vigenere_cipher'
+
+describe Cipher do
+  describe 'setting the key' do
+    it 'has generates a random key made up of 100 letters' do
+      key = Cipher.new.key
+
+      expect(key).to match(/^[a-z]+$/)
+      expect(key.length).to eq 100
+    end
+
+    it 'allows setting a key manually' do
+      key = 'abc'
+      cipher = Cipher.new(key)
+
+      expect(cipher.key).to eq key
+    end
+
+    it 'raises an error with an all caps key' do
+      expect { Cipher.new('ABCDE') }.to raise_error ArgumentError, 'Bad key'
+    end
+
+    it 'raises an error with a numeric key' do
+      expect { Cipher.new('12345') }.to raise_error ArgumentError, 'Bad key'
+    end
+
+    it 'raises an error with an empty key' do
+      expect { Cipher.new('') }.to raise_error ArgumentError, 'Bad key'
+    end
+  end
+
+  describe 'encoding and decoding' do
+    let(:key) { 'abcdefghij'}
+    let(:cipher) { Cipher.new(key) }
+
+    it 'can encode' do
+      expect(cipher.encode('aaaaaaaaaa')).to eq 'abcdefghij'
+    end
+
+    it 'can decode' do
+      expect(cipher.decode('abcdefghij')).to eq 'aaaaaaaaaa'
+    end
+
+    it 'is reversible' do
+      plaintext = 'asdagsgjalsdjgkls';
+      expect(cipher.decode(cipher.encode(plaintext))).to eq plaintext
+    end
+
+    it 'can wrap on encode' do
+      expect(cipher.encode('zzzzzzzzzz')).to eq 'zabcdefghi'
+    end
+
+    it 'can wrap on decode' do
+      expect(cipher.decode('zabcdefghi')).to eq 'zzzzzzzzzz'
+    end
+
+    it 'can handle messages longer than the key' do
+      expect(Cipher.new('abc').encode('iamapandabear'))
+        .to eq 'iboaqcnecbfcr'
+    end
+  end
+end
